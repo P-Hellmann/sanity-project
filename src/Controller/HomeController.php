6fc70207ee30fca3068a74819ba40654a7b63ba1
P-Hellmann <302053776@student.rocmondriaan.nl>
2025-02-05 11:53:59 +0100

    basis layout

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,11 @@ final class HomeController extends AbstractController
 
         ]);
     }
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig', [
+
+        ]);
+    }
 }
